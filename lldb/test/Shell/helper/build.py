ea1082cc8684ab562acf33b3418fb553c0aa902d
Eli Friedman <efriedma@qti.qualcomm.com>
2026-07-31 15:08:41 -0700

    [lldb] Make `%build` respect LLDB_TEST_COMPILER/LLDB_TEST_TRIPLE
    Currently, LLDB_TEST_COMPILER and LLDB_TEST_TRIPLE only apply to "API"
    tests; the "shell" code searches for a compiler on its own, in a few
    different ways. And there's no way to control what compiler is invoked,
    or how it's invoked.
    
    This patch threads LLDB_TEST_COMPILER and LLDB_TEST_TRIPLE through the
    configuration so it's used for tests which use `%build` (build.py).
    
    This doesn't touch %clang/%clang_host/etc.; maybe they should also be
    changed, but leave them alone for now.
    
    Fixes #203090.

--- lldb/test/Shell/helper/build.py
+++ lldb/test/Shell/helper/build.py
@@ -36,6 +36,14 @@ parser.add_argument(
 )
 
 parser.add_argument(
+    "--triple",
+    metavar="triple",
+    dest="triple",
+    required=True,
+    help="Target triple to pass to clang",
+)
+
+parser.add_argument(
     "--libs-dir",
     metavar="directory",
     dest="libs_dir",
@@ -296,6 +304,7 @@ class Builder(object):
         self.obj_ext = obj_ext
         self.lib_paths = args.libs_dir
         self.std = args.std
+        self.triple = args.triple
         assert (
             not args.objc_gnustep or args.objc_gnustep_dir
         ), "--objc-gnustep specified without path to libobjc2"
@@ -682,6 +691,9 @@ class MsvcBuilder(Builder):
         if self.std:
             args.append("/std:" + self.std)
 
+        if self.triple:
+            args.append("--target={0}".format(self.triple))
+
         args.append("/Fo" + obj)
         if self.toolchain_type == "clang-cl":
             args.append("--")
@@ -779,6 +791,9 @@ class GccBuilder(Builder):
 
         if self.std:
             args.append("-std={0}".format(self.std))
+
+        if self.triple:
+            args.append("--target={0}".format(self.triple))
 
         args.extend(["-o", obj])
         args.append(source)
--- lldb/test/Shell/helper/toolchain.py
+++ lldb/test/Shell/helper/toolchain.py
@@ -99,9 +99,8 @@ def use_lldb_substitutions(config):
     build_script = os.path.join(build_script, "build.py")
     build_script_args = [
         build_script,
-        (
-            "--compiler=clang" if config.enable_remote else "--compiler=any"
-        ),  # Default to best compiler
+        "--compiler=" + config.test_compiler,
+        *(["--triple="] if config.test_triple else []),
         "--arch=" + str(config.lldb_bitness),
     ]
     if config.lldb_lit_tools_dir:
